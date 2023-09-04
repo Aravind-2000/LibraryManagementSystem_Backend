0324151ca0238f1a240ef76131d40e6d0baab81d
Aravind-2000 <varavind2000@gmail.com>
2023-09-04 11:28:54 +0530

    Created a new email controller for testing of sending mails.

---
+++ src/main/java/com/library/prototype/Email/EmailController.java
@@ -0,0 +1,30 @@
+package com.library.prototype.Email;
+
+import com.library.prototype.Entity.BooksBorrowed;
+import com.library.prototype.Repository.BorrowedBooksRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.UnsupportedEncodingException;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+    private final BorrowedBooksRepository booksBorrowedService;
+
+    @GetMapping("/notification")
+    public void sendNotificationEmail() throws MessagingException, UnsupportedEncodingException {
+        BooksBorrowed booksBorrowed = booksBorrowedService.findById(1L).
+                    orElseThrow(() -> new RuntimeException("NO BORROWED BOOK FOUND IN THIS ID"));
+        emailService.sendMail_2(booksBorrowed.getUser().trim(), booksBorrowed);
+    }
+
+
+}
