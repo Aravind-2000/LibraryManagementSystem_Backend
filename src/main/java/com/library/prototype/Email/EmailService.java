1ec3ae5545629f22ccfa91fd55b50a5b132ed5e2
Aravind-2000 <varavind2000@gmail.com>
2023-09-04 11:27:00 +0530

    Added a new method for sending email without bodypart/multipart.

--- src/main/java/com/library/prototype/Email/EmailService.java
+++ src/main/java/com/library/prototype/Email/EmailService.java
@@ -3,15 +3,20 @@ package com.library.prototype.Email;
 
 import com.library.prototype.Entity.BooksBorrowed;
 import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
 import jakarta.mail.Multipart;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 import java.time.format.DateTimeFormatter;
 
 @Slf4j
@@ -25,7 +30,7 @@ public class EmailService {
 
     public void sendMail(String to, BooksBorrowed bookInfo){
         MimeMessagePreparator preparator = mimeMessage -> {
-            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(to.trim()));
             mimeMessage.setFrom(new InternetAddress("varavind2000@gmail.com"));
             mimeMessage.setSubject("Regarding the Due of a Borrowed Book");
             Multipart multipart = new MimeMultipart();
@@ -45,6 +50,27 @@ public class EmailService {
         try{
             mailSender.send(preparator);
             log.info("Mail Sent successfully");
+        }
+        catch (Exception e){
+            log.info(e.getMessage());
+        }
+    }
+
+    public void sendMail_2(String to, BooksBorrowed bookInfo) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Regarding the Due of a Borrowed Book";
+        String senderName = "Library Management Admin";
+        String emailContent = "The Book which you have borrowed '"+bookInfo.getBorrowedBook().getBookName()+"' has passed its due date (i.e) "
+                + bookInfo.getDueDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))+" " +
+                ", SO kindly return the book to the library or fine will be followed.";
+        MimeMessage message = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message);
+        messageHelper.setFrom("varavind2000@gmail.com", senderName);
+        messageHelper.setTo(to.trim());
+        messageHelper.setSubject(subject);
+        messageHelper.setText(emailContent, false);
+        try{
+            mailSender.send(message);
+            log.info("EMAIL SENT SUCCESSFULLY");
         }
         catch (Exception e){
             log.info(e.getMessage());
