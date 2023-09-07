89a07688b92e61e8433a36dcff8bfbcee4489a78
Aravind-2000 <varavind2000@gmail.com>
2023-09-07 10:32:59 +0530

    Modified some and scheduler worked successfully.

--- src/main/java/com/library/prototype/ScheduledTasks/ScheduleTask.java
+++ src/main/java/com/library/prototype/ScheduledTasks/ScheduleTask.java
@@ -6,6 +6,7 @@ import com.library.prototype.Entity.BooksBorrowed;
 import com.library.prototype.Repository.BorrowedBooksRepository;
 import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class ScheduleTask {
 
 
@@ -25,12 +27,11 @@ public class ScheduleTask {
     @Scheduled(cron = "0 30 10 * * ?") // Executes every day at 10.30 A.M if the due date is surpassed.
     public void invokeTask() throws MessagingException, UnsupportedEncodingException {
         List<BooksBorrowed> booksBorrowed = booksRepository.getAllValidBooks();
-        LocalDate currentDate = LocalDate.now();
         for (BooksBorrowed borrowed : booksBorrowed) {
-            if(borrowed.getBookStatus().equals(BookStatus.BORROWED)){
-                if (currentDate.equals(borrowed.getDueDate()) || currentDate.isAfter(borrowed.getDueDate())) {
-                    emailService.sendMail_2(borrowed.getUser(), borrowed);
-                }
+            if((borrowed.getBookStatus().equals(BookStatus.BORROWED)) &&
+                    (LocalDate.now().equals(borrowed.getDueDate()) || LocalDate.now().isAfter(borrowed.getDueDate()))){
+                emailService.sendMail_2(borrowed.getUser(), borrowed);
+                log.info("Scheduler worked successfully");
             }
         }
     }
